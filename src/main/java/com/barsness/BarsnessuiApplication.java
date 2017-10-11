728a946907b4384c55285052049fbfe25cef1e17
matt.barsness <matt.barsness@bluestembrands.com>
2017-10-11 16:10:45 -0500

    first commit

---
+++ src/main/java/com/barsness/BarsnessuiApplication.java
@@ -0,0 +1,12 @@
+package com.barsness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarsnessuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarsnessuiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/barsness/VaadinUI.java
@@ -0,0 +1,19 @@
+package com.barsness;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+/**
+ * Created by matt.barsness on 10/11/17.
+ */
+@SpringUI
+public class VaadinUI extends UI {
+
+
+    @Override
+    protected void init(VaadinRequest request) {
+
+
+    }
+}
